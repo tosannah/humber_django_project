720b0ac2749756793568d72b976a0675afc62c73
Oluwatosin <n10023936@humber.ca>
2026-08-18 14:22:21 -0400

    Implement Week 11 template language and catalog

--- waypoint/urls.py
+++ waypoint/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from .views import home, report, search
+from .views import home, report, search, catalog
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", home, name="home"),
     path("report/", report, name="report"),
     path("search/", search, name="search"),
+    path("catalog/", catalog, name="catalog"),
 ]
